53ed04b272d881c5f8a99072b7d977fe98650b5a
Devin Ho <djxho36@gmail.com>
2022-04-30 11:56:05 -0400

    commit tree 1650826ae0d3d3626374ab0711a1169b034ca78e

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+//   if(word.charAt(0) === word.charAt(word.length - 1)) {
+//     return true 
+//   } else  {
+//     return false
+//   }
+// }
+
+  return (word.charAt(0) === word.charAt(word.length - 1))
 }
 
 /* 
   Add your pseudocode here
+  Need an if statement for the letter of the word to equal the end of the letter of the word 
+  If the letters in the word matches then return otherwise (else) return false
 */
 
 /*
   Add written explanation of your solution here
+  CharAt(0) reads the first letter of the word 
+  CharAt(word.length) return the number of letters in the word and the -1 starts at the end of the word
+  then the if statement compares each letter
 */
 
 // You can run `node index.js` to view these console logs
